a5490b7a0fcdce916839e1af8b8e5cfaf6765345
Gilvando Gomes <58659255+Kadanda@users.noreply.github.com>
2024-10-21 10:15:33 -0300

    Create jsvan.js

---
+++ jsvan.js
@@ -0,0 +1,33 @@
+const habilidadesDesvantagens = [
+    {
+        nome: 'Adaptador',
+        pontos: -2,
+        descricao: 'Você tem facilidade para lidar com qualquer tipo de arma, sabendo usá-las mesmo que nunca as tenha visto. Entretanto não é capaz de usar tais outras armas tão bem quanto a do seu estilo de combate, mas ainda as usando com certa maestria. Pode ser adquirida nas aventuras, com 3 páginas de treinamento. Players sem essa vantagem tem seu Acerto/Mira reduzido em 50% ao usar armas que não sejam de seu estilo de combate.'
+    },
+    {
+        nome: 'Ambidestria',
+        pontos: -2,
+        descricao: 'Enquanto uns tem maestria com somente uma mão para fazer certas coisas, você, seu filho da mãe, sabe usar as duas mãos pra qualquer coisa. Pode ser adquirida nas aventuras, com 2 páginas de treino. Players sem essa vantagem tem seu Acerto/Mira reduzida em 50% e Dano/Ataque reduzida em 25% ao realizar ações com sua mão não hábil.'
+    },
+    {
+        nome: 'Amor Verdadeiro',
+        pontos: -4,
+        descricao: 'O personagem possui uma pessoa quem ela ama de verdade. Esse amor é tão forte, que ele se torna mais forte toda vez que vai proteger a pessoa amada, mesmo que o amor não seja recíproco. Pode ser adquirida nas aventuras (Conhecendo algum npc ou player) e na criação do personagem (Terá que criar um npc na história). Essa vantagem garante 20% de bônus em Defesa/Bloqueio e bônus de 10% nos outros atributos (Esquiva/Evasão e Acerto/Mira) em toda batalha que o usuário proteger a pessoa que ama.'
+    },
+    {
+        nome: 'Assexuado(a)',
+        pontos: -2,
+        descricao: 'As personagens que possuem essa vantagem, por uma razão muito estranha, não se interessam por sexo. Isso não quer dizer que elas nunca farão ou não poderão fazer, elas podem, mas isso é a última vontade delas em uma lista bem extensa. Por isso, não importa o quanto uma pessoa tente, nunca conseguirá seduzir uma pessoa com essa vantagem de primeira. Somente um pouco de convivência pode fazer com que essa pessoa se interesse sexualmente em outra. Essa vantagem não tem nada a ver com não gostar de certo sexo, quem a possui realmente não vai ligar de modo algum pra nenhum ser que tente seduzi-lo, seja homem ou mulher. Pode ser adquirida apenas na Criação do Personagem. Essa vantagem permite que a personagem seja imune à tentativas de sedução.'
+    },
+    {
+        nome: 'Beberrão',
+        pontos: -2,
+        descricao: 'Seu personagem não consegue recusar bebida alcoólica mas, felizmente, consegue beber mais que um boi. Não importa a quantidade de álcool que ingira, jamais ficará bêbado. Pode ser adquirida nas aventuras, tentando superar seus limites na bebida.'
+    },
+    {
+        nome: 'Boa Memória',
+        pontos: -2,
+        descricao: 'Sua memória é impressionante. Você consegue se lembrar do cheiro do perfume, qual era a cor da raiz do cabelo e o que ouviu numa noite com uma pessoa especial. Se você conversa com uma pessoa num encontro, no outro encontro você já lembra as preferências da outra pessoa, ou seja, consegue se lembrar dos mínimos detalhes. Pode ser adquirida apenas na Criação do Personagem. Não pode ser usada em conjunto com desvantagem Esquecido nem Falsas Lembranças.'
+    }
+    // Adicione as outras habilidades e desvantagens aqui
+];
